8375e7986df03168d9a1622b2e251861447adaf8
wujy <wujunyin@dxy.cn>
2022-04-02 11:17:01 +0800

    refactor: 改变目录组织

--- demo/background.js
+++ demo/background.js
@@ -1,6 +1,7 @@
-let color = '#3aa757';
+let color = "#3aa757";
 
+// 插件加载后，向后台沙箱注入代码和变量
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  console.log("Default background color set to %cgreen", `color: ${color}`);
 });
--- demo/popup.js
+++ demo/popup.js
@@ -1,4 +1,3 @@
-// Initialize butotn with users's prefered color
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
@@ -6,17 +5,21 @@ chrome.storage.sync.get("color", ({ color }) => {
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
+// 当点击按钮时，将setPageBackgroundColor注入当前页面
 changeColor.addEventListener("click", async () => {
+  // 获取当前 window 窗口和激活状态的 tab 页
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+  // 想目标 tag 注入方法 setPageBackgroundColor
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: setPageBackgroundColor,
   });
 });
 
-// The body of this function will be execuetd as a content script inside the
+// The body of this function will be executed as a content script inside the
 // current page
+// 该方法将作为内容脚本注入当前页面
 function setPageBackgroundColor() {
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
