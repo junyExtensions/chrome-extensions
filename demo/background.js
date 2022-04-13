4501cacfd5f5b53aaa747a7dc7f46e37dd3c0b5d
wujy <wujunyin@dxy.cn>
2022-04-13 14:09:57 +0800

    revert: remove demo

--- demo/background.js
+++
@@ -1,7 +0,0 @@
-let color = "#3aa757";
-
-// 插件加载后，向后台沙箱注入代码和变量
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log("Default background color set to %cgreen", `color: ${color}`);
-});
